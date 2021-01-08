da4fe12ad0c74ee304ab7806d01fa0797b25a786
Eisuke Ishikawa <eisuke@mac.local>
2021-01-08 16:26:18 +0900

    theme, It works

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppBar from '@material-ui/core/AppBar'
 import { Container, CssBaseline, Grid, Toolbar, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles'
 
 import Icon from '../public/images/icon.jpg'
+import { teal } from '@material-ui/core/colors'
 
 const useStyles = makeStyles((theme) => ({
   hero: {
@@ -16,30 +17,44 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid black"
   }
 }))
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[500]
+    },
+    secondary: {
+      main: '#00bcd4'
+    }
+  }
+})
+
 function App() {
   const classes = useStyles()
   return (
     <>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            axxeix
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <div className={classes.hero}>
-          <Container maxWidth="sm">
-            <Grid container alignItems="center" justify="center">
-              <img src={Icon} alt="アイコン画像" className={classes.image}></img>
-            </Grid>
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative" color="primary">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
               axxeix
             </Typography>
-          </Container>
-        </div>
-      </main>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div className={classes.hero}>
+            <Container maxWidth="sm">
+              <Grid container alignItems="center" justify="center">
+                <img src={Icon} alt="アイコン画像" className={classes.image}></img>
+              </Grid>
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                axxeix
+              </Typography>
+            </Container>
+          </div>
+        </main>
+      </ThemeProvider>
     </>
   )
 }
